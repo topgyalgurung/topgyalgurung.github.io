1ce871aa66f5d3dea0282c1bc1265fccc23a8c5f
Topgyal Gurung <topgyaltsering3@gmail.com>
2025-06-02 16:00:21 -0400

    hamburger menu

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// hamburger menu
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.getElementById("hamburger");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  if (hamburger && mobileMenu) {
+    hamburger.addEventListener("click", () => {
+      mobileMenu.classList.toggle("hidden");
+    });
+  }
+});
+
 // const observer = new IntersectionObserver((entries) => {
 //   entries.forEach((entry) => {
 //     console.log(entry);
